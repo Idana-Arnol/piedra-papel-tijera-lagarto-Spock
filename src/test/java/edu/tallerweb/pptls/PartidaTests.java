b07f10f97a94765bb1e6543551b39916df5fe7e9
Idana-Arnol <arnol.idana@gmail.com>
2013-10-01 18:16:43 -0300

    Modificación del Test

--- src/test/java/edu/tallerweb/pptls/PartidaTests.java
+++ src/test/java/edu/tallerweb/pptls/PartidaTests.java
@@ -11,9 +11,48 @@ public class PartidaTests {
 		Mano jugadorUno = new Mano(Forma.SPOCK);
 		Mano jugadorDos = new Mano(Forma.PIEDRA);
 
-		assertEquals("Spock vaporiza piedra",
+		assertEquals("Spock vaporiza piedra", 
 				Resultado.GANA, jugadorUno.jugarCon(jugadorDos));
+	}
 
+	@Test
+	public void quePiedraAplastaLagarto() {
+	
+		Mano jugadorUno = new Mano(Forma.PIEDRA);
+		Mano jugadorDos = new Mano(Forma.LAGARTO);
+	
+		assertEquals("Piedra no aplasta lagarto",
+				Resultado.PIERDE, jugadorDos.jugarCon(jugadorUno));
+	}
+	
+	@Test
+	public void queTijeraCortaPapel() {
+
+		Mano jugadorUno = new Mano(Forma.TIJERA);
+		Mano jugadorDos = new Mano(Forma.TIJERA);
+		
+		assertEquals("Tijera empata Tijera", 
+				Resultado.EMPATA, jugadorUno.jugarCon(jugadorDos));
+	}	
+	
+	@Test
+	public void queLagartoEnvenenaSpock() {
+
+		Mano jugadorUno = new Mano(Forma.LAGARTO);
+		Mano jugadorDos = new Mano(Forma.LAGARTO);
+
+		assertEquals("Lagarto empata LAGARTO", 
+				Resultado.EMPATA, jugadorUno.jugarCon(jugadorDos));
+	}
+	
+	@Test
+	public void quePapelCubrePiedra() {
+
+		Mano jugadorUno = new Mano(Forma.PAPEL);
+		Mano jugadorDos = new Mano(Forma.PIEDRA);
+
+		assertEquals("Papel cubre Piedra", 
+				Resultado.GANA, jugadorUno.jugarCon(jugadorDos));
 	}
 
 }
